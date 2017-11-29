0a1c81d0060de35946056fd3238470d5b4ec5569
Meejin <mchoi1215@berkeley.edu>
2017-11-29 03:38:59 -0800

    done with hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,15 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    # changes each string into an integer, removes any duplicates
+    changes = a.uniq.map{|elem| elem.to_i}
+    # adds two to each number 
+    adds = changes.map{|elem| elem += 2}
+    # keeps the even numbers only, rejects the resulting numbers greater than ten
+    keeps = adds.select{|elem| elem.even? and elem<10}
+    # finally returns the sum of the resulting array
+    keeps.inject(0){|sum,x| sum + x}
   end
 end
 
